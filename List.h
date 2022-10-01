52c8ef27a1657923f781edab063cc7240dce66f7
Minh <bhminh322@gmail.com>
2022-10-01 16:53:47 +0700

    add new template List

---
+++ List.h
@@ -0,0 +1,87 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+class List
+{
+private:
+  vector<T> list;
+
+public:
+  List(/* args */);
+  ~List();
+  int length();
+  void add(const T &);
+  void insert(const T &, int);
+  int indexOf(T &);
+  T at(int index);
+  void removeAt(int index);
+  void remove(T &);
+  void update(int);
+  void show();
+};
+template <class T>
+List<T>::List(/* args */)
+{
+}
+template <class T>
+List<T>::~List()
+{
+}
+template <class T>
+void List<T>::add(const T &t)
+{
+  this->list.push_back(t);
+}
+template <class T>
+int List<T>::length()
+{
+  return this->list.size();
+}
+template <class T>
+void List<T>::insert(const T &t, int index)
+{
+  this->list.insert(list.begin() + index, t);
+}
+template <class T>
+int List<T>::indexOf(T &t)
+{
+  for (int i = 0; i < list.size(); i++)
+  {
+    if (list[i] == t)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+template <class T>
+T List<T>::at(int index)
+{
+  return list[index];
+}
+template <class T>
+void List<T>::removeAt(int index)
+{
+  list.erase(list.begin() + index);
+}
+template <class T>
+void List<T>::remove(T &t)
+{
+  int index = this->indexOf(t);
+  if (index == -1)
+    return;
+  this->removeAt(index);
+}
+
+template <class T>
+void List<T>::show()
+{
+  cout << endl;
+  for (int i = 0; i < list.size(); i++)
+  {
+    cout << list[i] << endl;
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-#include "ListComputer.h"
+// #include "ListComputer.h"
+#include "Computer.h"
+#include "List.h"
+#include "Student.h"
 using namespace std;
 
 int main()
 {
   Location location(2, 4);
-  ListComputer list;
   Computer lenovo("1", false, location, "0", "102210116");
   Computer lenovo2("1", false, location, "0", "2");
+  List<Computer> list;
+  List<Student> listStudent;
   bool kq = (lenovo2 == lenovo);
   list.add(lenovo);
-  // list.show();
+  list.show();
   cout << kq;
   return 0;
 }
